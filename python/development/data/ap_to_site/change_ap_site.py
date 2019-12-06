4d09ac45c5d5fc1ee68296e0f26a9b7b37239401
fsedanoc <fran@fransedano.net>
2019-12-06 18:51:12 +0100

    Update code to match docs

--- python/development/data/ap_to_site/change_ap_site.py
+++ python/development/data/ap_to_site/change_ap_site.py
@@ -5,14 +5,18 @@ Code to provision AP tags based on spreadsheet data
 
 import logging
 from readinventory import Inventory
-from wirelesscontroller import wirelesscontroller
+from wirelesscontroller import C9800
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s (%(levelname)s) %(message)s")
 
 ### Replace IP with the one for your controller
 controller = {
-    "ip":"<your_controller_ip>",
-    "user":"lab",
-    "password":"lab"
+    "ip":"<your_ip_address>",
+    "user":"admin",
+    "password":"Vimlab123@"
 }
+
+wlc = C9800(controller["ip"],
+    controller["user"],
+    controller["password"])
 
